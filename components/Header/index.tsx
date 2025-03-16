909bb9c3cd313a55d89d619ff25e6dbb72374674
fujinraijinog <fujinraijin@gmail.com>
2025-03-16 16:12:55 -0500

    Remove unused backup components and clean up header functionality

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -31,9 +31,7 @@ const Header = () => {
     <>
       <header
         className={`fixed left-0 top-0 z-50 w-full py-7 transition-all ${
-          stickyMenu
-            ? "bg-white py-4 shadow dark:bg-black"
-            : ""
+          stickyMenu ? "bg-white py-4 shadow dark:bg-black" : ""
         }`}
       >
         <div className="relative mx-auto flex max-w-c-1390 items-center justify-between px-4 md:px-8 xl:flex 2xl:px-0">
@@ -123,7 +121,7 @@ const Header = () => {
                       </>
                     ) : (
                       <Link
-                        href={menuItem.path}
+                        href={menuItem.path ? String(menuItem.path) : "#"}
                         className={pathUrl === menuItem.path ? "text-primary hover:text-primary" : "hover:text-primary"}
                       >
                         {menuItem.title}
@@ -135,29 +133,6 @@ const Header = () => {
             </nav>
 
             <div className="ml-auto mt-7 flex items-center gap-6 xl:mt-0">
-              <button
-                onClick={() => setSearchModalOpen(true)}
-                className="hidden h-[38px] w-[38px] items-center justify-center rounded-full bg-white text-waterloo dark:bg-black sm:flex"
-                aria-label="Open search"
-              >
-                <svg
-                  width="20"
-                  height="20"
-                  viewBox="0 0 18 18"
-                  fill="currentColor"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <g clipPath="url(#clip0_369_1884)">
-                    <path d="M16.9347 15.3963L12.4816 11.7799C14.3168 9.26991 14.1279 5.68042 11.8338 3.41337C10.6194 2.19889 9.00003 1.52417 7.27276 1.52417C5.54549 1.52417 3.92617 2.19889 2.71168 3.41337C0.201738 5.92332 0.201738 10.0256 2.71168 12.5355C3.92617 13.75 5.54549 14.4247 7.27276 14.4247C8.91907 14.4247 10.4574 13.804 11.6719 12.6975L16.179 16.3409C16.287 16.4219 16.4219 16.4759 16.5569 16.4759C16.7458 16.4759 16.9077 16.3949 17.0157 16.26C17.2316 15.9901 17.2046 15.6122 16.9347 15.3963ZM7.27276 13.2102C5.86935 13.2102 4.5739 12.6705 3.57532 11.6719C1.52418 9.62076 1.52418 6.30116 3.57532 4.27701C4.5739 3.27843 5.86935 2.73866 7.27276 2.73866C8.67617 2.73866 9.97162 3.27843 10.9702 4.27701C13.0213 6.32815 13.0213 9.64775 10.9702 11.6719C9.99861 12.6705 8.67617 13.2102 7.27276 13.2102Z" />
-                  </g>
-                  <defs>
-                    <clipPath id="clip0_369_1884">
-                      <rect width="17.2727" height="17.2727" fill="white" transform="translate(0.363647 0.363647)" />
-                    </clipPath>
-                  </defs>
-                </svg>
-              </button>
-
               <ThemeToggler />
 
               {session ? (
